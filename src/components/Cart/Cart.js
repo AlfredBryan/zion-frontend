8a68e1791a8525fb7ec55e9487d3c1565b1e52a7
Alfred Chimereze <alfred.chimereze@gmail.com>
2020-04-20 14:21:50 +0100

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,105 +16,105 @@ class Cart extends Component {
     itemTotal: 0,
   };
 
-  increment = (id) => {
-    let tempCart = this.state.cart.map((products) =>
-      products.map((newProduct) =>
-        newProduct.map((updatedProduct) => updatedProduct)
-      )
-    );
-    const selectedProduct = tempCart.find((item) => item._id === id);
+  // increment = (id) => {
+  //   let tempCart = this.state.cart.map((products) =>
+  //     products.map((newProduct) =>
+  //       newProduct.map((updatedProduct) => updatedProduct)
+  //     )
+  //   );
+  //   const selectedProduct = tempCart.find((item) => item._id === id);
 
-    const index = tempCart.indexOf(selectedProduct);
-    const product = tempCart[index];
+  //   const index = tempCart.indexOf(selectedProduct);
+  //   const product = tempCart[index];
 
-    product.count = product.count + 1;
-    product.total = product.count * product.price;
+  //   product.count = product.count + 1;
+  //   product.total = product.count * product.price;
 
-    this.setState(
-      () => {
-        return { cart: [...tempCart] };
-      },
-      () => {
-        this.addTotals();
-      }
-    );
-  };
+  //   this.setState(
+  //     () => {
+  //       return { cart: [...tempCart] };
+  //     },
+  //     () => {
+  //       this.addTotals();
+  //     }
+  //   );
+  // };
 
-  decrement = (id) => {
-    let tempCart = [...this.state.cart];
-    const selectedProduct = tempCart.find((item) => item._id === id);
+  // decrement = (id) => {
+  //   let tempCart = [...this.state.cart];
+  //   const selectedProduct = tempCart.find((item) => item._id === id);
 
-    const index = tempCart.indexOf(selectedProduct);
-    const product = tempCart[index];
+  //   const index = tempCart.indexOf(selectedProduct);
+  //   const product = tempCart[index];
 
-    product.count = product.count - 1;
-    if (product.count === 0) {
-      this.removeItem(id);
-    } else {
-      product.total = product.count * product.price;
+  //   product.count = product.count - 1;
+  //   if (product.count === 0) {
+  //     this.removeItem(id);
+  //   } else {
+  //     product.total = product.count * product.price;
 
-      this.setState(
-        () => {
-          return { cart: [...tempCart] };
-        },
-        () => {
-          this.addTotals();
-        }
-      );
-    }
-  };
+  //     this.setState(
+  //       () => {
+  //         return { cart: [...tempCart] };
+  //       },
+  //       () => {
+  //         this.addTotals();
+  //       }
+  //     );
+  //   }
+  // };
 
-  removeItem = (id) => {
-    let tempProducts = [...this.state.products];
-    let tempCart = [...this.state.cart];
+  // removeItem = (id) => {
+  //   let tempProducts = [...this.state.products];
+  //   let tempCart = [...this.state.cart];
 
-    tempCart = tempCart.filter((item) => item._id !== id);
+  //   tempCart = tempCart.filter((item) => item._id !== id);
 
-    const index = tempProducts.indexOf(this.getItem(id));
-    let removedProduct = tempProducts[index];
-    removedProduct.inCart = false;
-    removedProduct.count = 0;
-    removedProduct.total = 0;
+  //   const index = tempProducts.indexOf(this.getItem(id));
+  //   let removedProduct = tempProducts[index];
+  //   removedProduct.inCart = false;
+  //   removedProduct.count = 0;
+  //   removedProduct.total = 0;
 
-    this.setState(
-      () => {
-        return {
-          cart: [...tempCart],
-          products: [...tempProducts],
-        };
-      },
-      () => {
-        this.addTotals();
-      }
-    );
-  };
+  //   this.setState(
+  //     () => {
+  //       return {
+  //         cart: [...tempCart],
+  //         products: [...tempProducts],
+  //       };
+  //     },
+  //     () => {
+  //       this.addTotals();
+  //     }
+  //   );
+  // };
 
-  clearCart = () => {
-    this.setState(
-      () => {
-        return { cart: [] };
-      },
-      () => {
-        this.setProducts();
-        this.addTotals();
-      }
-    );
-  };
+  // clearCart = () => {
+  //   this.setState(
+  //     () => {
+  //       return { cart: [] };
+  //     },
+  //     () => {
+  //       this.setProducts();
+  //       this.addTotals();
+  //     }
+  //   );
+  // };
 
-  addTotals = () => {
-    let subTotal = 0;
-    this.state.cart.map((item) => (subTotal += item.total));
-    const tempTax = subTotal * 0.1;
-    const tax = parseFloat(tempTax.toFixed(2));
-    const total = subTotal + tax;
-    this.setState(() => {
-      return {
-        cartSubTotal: subTotal,
-        cartTax: tax,
-        cartTotal: total,
-      };
-    });
-  };
+  // addTotals = () => {
+  //   let subTotal = 0;
+  //   this.state.cart.map((item) => (subTotal += item.total));
+  //   const tempTax = subTotal * 0.1;
+  //   const tax = parseFloat(tempTax.toFixed(2));
+  //   const total = subTotal + tax;
+  //   this.setState(() => {
+  //     return {
+  //       cartSubTotal: subTotal,
+  //       cartTax: tax,
+  //       cartTotal: total,
+  //     };
+  //   });
+  // };
 
   viewCart = () => {
     const token = localStorage.getItem("token");
@@ -204,19 +204,9 @@ class Cart extends Component {
                           <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
                             <div className="d-flex adjust_btn justify-content-center">
                               <div>
-                                <span
-                                  className="btn btn-black mx-1"
-                                  onClick={() => this.decrement(newProduct._id)}
-                                >
-                                  -
-                                </span>
+                                <span className="btn btn-black mx-1">-</span>
                                 <span className="btn btn-black mx-1">{}</span>
-                                <span
-                                  className="btn btn-black mx-1"
-                                  onClick={() => this.increment(newProduct._id)}
-                                >
-                                  +
-                                </span>
+                                <span className="btn btn-black mx-1">+</span>
                               </div>
                             </div>
                           </div>
